eb5f807da82366952905d6f9b2cab1edd76405c7
mongoobuild <mongoo@sina.cn>
2014-03-26 14:08:50 +0800

    Update mt.py

--- mt/mt.py
+++ mt/mt.py
@@ -1,4 +1,4 @@
-#encoding=utf8
+# -*- coding: utf-8 -*-
 from collections import defaultdict
 
 
@@ -8,45 +8,51 @@ class Trans(object):
 
     IBM - model 1 EM算法模型
 
-    e: 原语言
-    f: 目标语言
+    source: 原语言
+    target: 目标语言
+
+    >>> model = Trans(['i laugh', 'laugh loudly'], ['我 笑', '大声地 笑'])
+    >>> print round(model['i']['我'], 3) # before
+    0.5
+    >>> model.em(10)
+    >>> print round(model['i']['我'], 3) # after
+    0.998
     """
-    def __init__(self, e, f):
+    def __init__(self, source, target, n=0):
         """
-        e: 原语言
-        f: 目标语言
-        t: (e|f)
+        source: 原语言
+        target: 目标语言
+        ttable: (e|f)
         """
-        self.e = e
-        self.f = f
-        self.t = defaultdict(lambda: defaultdict(float))
-        for (ei, fi) in self._read(self.e, self.f):
-            for ew in ei:
-                for fw in fi:
-                    self.t[ew][fw] += 1
+        self.source = source
+        self.target = target
+        self.ttable = defaultdict(lambda: defaultdict(float))
+        for (s, t) in self._read(self.source, self.target):
+            for sw in s:
+                for tw in t:
+                    self.ttable[sw][tw] += 1
         self._normalize()
-        self.n = 0
 
     def __getitem__(self, item):
-        return self.t[item]
+        return self.ttable[item]
 
-    def _read(self, e_path, f_path):
+    def _read(self, source, target):
         """
         读取平行文本
         """
-        e = e_path if hasattr(e_path, 'read') else open(e_path, 'r')
-        f = f_path if hasattr(f_path, 'read') else open(f_path, 'r')
-        for (ei, fi) in zip(e, f):
-            yield ([None] + ei.strip().split(), fi.strip().split())
+        sourcef = source if isinstance(source, list) else open(source, 'r')
+        targetf = target if isinstance(target, list) else open(target, 'r')
+        for (s, t) in zip(sourcef, targetf):
+            yield ([None] + s.strip().split(), t.strip().split())
 
     def _normalize(self):
         """
         规格化
         """
-        for (ew, fw_t) in self.t.iteritems():
-            Z = sum(fw_t.values())
-            for fw in fw_t:
-                fw_t[fw] = fw_t[fw] / Z
+        for (sw, t) in self.ttable.iteritems():
+            Z = sum(t.values())
+            for tw in t:
+                t[tw] = t[tw] / Z
 
     def em(self, n=1):
         """
@@ -56,17 +62,17 @@ class Trans(object):
             count = defaultdict(float)
             total = defaultdict(float)
             #E步
-            for (e, f) in self._read(self.e, self.f):
-                for ew in e:
-                    for fw in f:
-                        c = self.t[ew][fw]
-                        count[(ew, fw)] += c
-                        total[fw] += c
+            for (s, t) in self._read(self.source, self.target):
+                for sw in s:
+                    for tw in t:
+                        c = self.ttable[sw][tw]
+                        count[(sw, tw)] += c
+                        total[tw] += c
             #M步
-            for ((ew, fw), val) in count.iteritems():
-                self.t[ew][fw] = val / total[fw]
+            for ((sw, tw), val) in count.iteritems():
+                self.ttable[sw][tw] = val / total[tw]
             self._normalize()
-            self.n += 1
+            # self.n += 1
 
 
 class Lang(object):
@@ -74,36 +80,40 @@ class Lang(object):
     语言模型
 
     上下文无关概率模型
+
+    >>> lm = Lang(['我 笑', '大声地 笑'])
+
+    >>> print round(lm['我'], 3)
+    0.25
     """
-    def __init__(self, f):
+    def __init__(self, target):
         """
-        f: 目标语言
+        target: 目标语言
         """
-        self.f = f
-        self.p = defaultdict(float)
-        for fi in self._read(self.f):
-            for fw in fi:
-                self.p[fw] += 1
+        self.table = defaultdict(float)
+        for t in self._read(target):
+            for tw in t:
+                self.table[tw] += 1
         self._normalize()
 
     def __getitem__(self, item):
-        return self.p[item]
+        return self.table[item]
 
-    def _read(self, f_path):
+    def _read(self, target):
         """
         读取目标语言文本(若需分词, 已分词)
         """
-        f = f_path if hasattr(f_path, 'read') else open(f_path, 'r')
-        for fi in f:
-            yield fi.strip().split()
+        targetf = target if isinstance(target, list) else open(target, 'r')
+        for t in targetf:
+            yield t.strip().split()
 
     def _normalize(self):
         """
         规则化
         """
-        Z = len(self.p)
-        for (fw, val) in self.p.iteritems():
-            self.p[fw] = val / Z
+        Z = sum(self.table.values())
+        for (tw, pval) in self.table.iteritems():
+            self.table[tw] = pval / Z
 
 
 class Decode(object):
@@ -112,32 +122,35 @@ class Decode(object):
 
     全局搜索
 
-    t: 翻译模型对象
-    l: 语言模型对象
-    s: 需要翻译字符串
+    ttable: 词对齐概率表
+    lm: 语言模型概率表
+    source: 需要翻译字符串
     """
-    def __init__(self, t, l, s):
-        self.t = t
-        self.l = l
-        self.s = s
-        self._decode()
+    def __init__(self, ttable, lm, source):
+        self.ttable = ttable
+        self.lm = lm
+        self.source = source
+        self.decode_training()
 
-    def _decode(self):
-        fw_t = self.t[self.s]
-        print fw_t
-        print len(fw_t)
-        print '------'
-        f = ''
-        pr = 0
-        for fw in fw_t:
-            print fw_t[fw], "\t", self.l[fw], "\t", fw_t[fw] * self.l[fw], "\t", fw
-            if fw_t[fw] * self.l[fw] > pr:
-                pr = fw_t[fw] * self.l[fw]
-                f = fw
-        print self.s, '--', f
+    def decode_training(self):
+        target = None
+        best_p = 0
+        for (tw, pval) in self.decode_pair():
+            if pval > best_p:
+                best_p = pval
+                target = tw
+        print "%s -- %s : %f" % (self.source, target, round(best_p, 5))
+
+    def decode_pair(self):
+        t = self.ttable[self.source]
+        for tw in t:
+            yield (tw, t[tw] * self.lm[tw])
+
 
 if __name__ == '__main__':
-    t = Trans('data/e.small', 'data/f.small')
-    t.em(10)
-    l = Lang('data/f.small')
-    Decode(t, l, 'LETTER')
+    import doctest
+    doctest.testmod()
+
+    # m1 = Trans('data/en', 'data/fr', 10)
+    # lm = Lang('data/fr')
+    # Decode(m1, lm, 'THE')
